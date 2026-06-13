11fc6c4c53d33431c1d87f3b09a33ae367519993
Arpitha@1212 <mbarpitha07@gmail.com>
2026-06-13 23:15:53 +0530

    Add Server.java

---
+++ Server.java
@@ -0,0 +1,32 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    public void run()throws IOException{
+        int port = 8080;
+        ServerSocket serverSocket = new ServerSocket(port);
+        serverSocket.setSoTimeout(10000);
+        while(true){ 
+            try {
+            System.out.println("server is listen for this port : "+ port);
+            Socket acceptedConnection = serverSocket.accept();
+            System.out.println("Connection accepted from client : " + acceptedConnection.getRemoteSocketAddress());
+            PrintWriter toClient = new PrintWriter(acceptedConnection.getOutputStream());
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(acceptedConnection.getInputStream()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+           
+        }
+    }
+    
+    public static void main(String[] args){
+
+    }
+}
